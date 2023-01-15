4534e7dc3ca623663bee7846aec48759393e8821
baswanth papisetty <p.baslass@gmail.com>
2023-01-15 18:23:10 +0530

    as

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const io = socketio(server);
 const router = express.Router();
 
 app.use((req, res, next)=> {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", ["http://localhost:3000"]);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header('Access-Control-Allow-Credentials', false);
   next();
