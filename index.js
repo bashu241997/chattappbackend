d7cee41f1047d3aa0ac0e88a54995db6b73ad459
baswanth papisetty <p.baslass@gmail.com>
2023-01-15 18:25:24 +0530

    data

--- index.js
+++ index.js
@@ -12,11 +12,10 @@ const router = express.Router();
 app.use((req, res, next)=> {
   res.header("Access-Control-Allow-Origin", ["http://localhost:3000"]);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header('Access-Control-Allow-Credentials', false);
   next();
 });
 router.get("/", (req, res) => {
-  res.send( "chat app datas").status(200);
+  res.send( "Welcome to back end of chat app").status(200);
 });
 app.use(router);
 
